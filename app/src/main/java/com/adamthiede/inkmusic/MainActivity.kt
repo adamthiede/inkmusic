b560f8c86e8e6dd6fc38538bfb6250e9cbba729d
adamthiede <me@adamthiede.com>
2025-07-13 15:05:44 -0500

    styling tweaks

--- app/src/main/java/com/adamthiede/inkmusic/MainActivity.kt
+++ app/src/main/java/com/adamthiede/inkmusic/MainActivity.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+//import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.adamthiede.inkmusic.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,20 +16,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Hide the action bar (header bar)
+        supportActionBar?.hide()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+        // Always show labels on the bottom bar
+        navView.labelVisibilityMode = NavigationBarView.LABEL_VISIBILITY_LABELED
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_nowplaying, R.id.navigation_artists, R.id.navigation_albums, R.id.navigation_songs
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_nowplaying, R.id.navigation_artists, R.id.navigation_albums, R.id.navigation_songs
+//            )
+//        )
+        // Remove setupActionBarWithNavController to avoid showing the header bar
+        // setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
